fb2113fe8b037806904b33063294958861415f06
ericstober <erictstober@gmail.com>
2024-03-08 15:34:26 -0600

    added search function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 const global = {
   currentPage: window.location.pathname,
+  search: {
+    term: '',
+    type: '',
+    page: 1,
+    totalPages: 1
+  },
+  api : {
+    // API key registered at https://www.themoviedb.org/settings/api
+    // Only use for devlopment. For production store the key and make requests from server
+    apiKey: '7b2d525832e71dc1b4d600029324fc93',
+    apiUrl: 'https://api.themoviedb.org/3/'
+  }
 };
 
 // Display 20 most popular movies
@@ -228,6 +240,66 @@ function displayBackgroundImage(type, backgroundPath) {
   }
 }
 
+// Search Movies/TV Shows
+async function search() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+
+  global.search.type = urlParams.get('type');
+  global.search.term = urlParams.get('search-term');
+
+  if (global.search.term !== '' && global.search.term !== null) {
+    const { results, total_pages, page } = await searchAPIData();
+
+    if (results.length === 0) {
+      showAlert('No results found');
+      return;
+    } else {
+      displaySearchResults(results);
+    }
+
+    document.querySelector('#search-term').value = '';
+
+  } else {
+    showAlert('Please enter a search term');
+  }
+}
+
+// Display search results
+function displaySearchResults(results) {
+  results.forEach((result) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+      <a href="${global.search.type}-details.html?id=${result.id}">
+        ${
+          result.poster_path
+            ? `
+          <img
+            src="https://image.tmdb.org/t/p/w500/${result.poster_path}"
+            class="card-img-top"
+            alt="${global.search.type === 'movie' ? result.title : result.name}"
+          />`
+            : `
+          <img
+            src="images/no-image.jpg"
+            class="card-img-top"
+            alt="${global.search.type === 'movie' ? result.title : result.name}"
+          />`
+        }
+      </a>
+      <div class="card-body">
+        <h5 class="card-title">${global.search.type === 'movie' ? result.title : result.name}</h5>
+        <p class="card-text">
+          <small class="text-muted">Release: ${global.search.type === 'movie' ? result.release_date : result.first_air_date}</small>
+        </p>
+      </div>
+    `;
+
+    document.querySelector('#search-results').appendChild(div);
+  });
+}
+
 // Display Slider Movies
 async function displaySlider() {
   const { results } = await fetchAPIData('movie/now_playing');
@@ -278,10 +350,8 @@ function initSwiper() {
 
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
-  // API key registered at https://www.themoviedb.org/settings/api
-  // Only use for devlopment. For production store the key and make requests from server
-  const API_KEY = "7b2d525832e71dc1b4d600029324fc93";
-  const API_URL = "https://api.themoviedb.org/3/";
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiUrl;
 
   showSpinner();
 
@@ -296,10 +366,30 @@ async function fetchAPIData(endpoint) {
   return data;
 }
 
+// Make request to search
+async function searchAPIData() {
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiUrl;
+
+  showSpinner();
+
+  const response = await fetch(
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
+  );
+
+  const data = await response.json();
+
+  hideSpinner();
+
+  return data;
+}
+
+// Show loading spinner
 function showSpinner() {
   document.querySelector('.spinner').classList.add('show');
 }
 
+// Hide loading spinner
 function hideSpinner() {
   document.querySelector('.spinner').classList.remove('show');
 }
@@ -312,6 +402,16 @@ function highlightActiveLink() {
       link.classList.add("active");
     }
   });
+}
+
+// Show Alert
+function showAlert(message, className = 'error') {
+  const alertElement = document.createElement('div');
+  alertElement.classList.add('alert', className);
+  alertElement.appendChild(document.createTextNode(message));
+  document.querySelector('#alert').appendChild(alertElement);
+
+  setTimeout(() => alertElement.remove(), 3000);
 }
 
 // Add commas to budget and revenue dollar values
@@ -337,7 +437,7 @@ function init() {
       displayShowDetails();
       break;
     case "/search.html":
-      console.log("Search");
+      search();
       break;
   }
 
